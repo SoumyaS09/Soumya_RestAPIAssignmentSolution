c68244082cd78ca40a26125084d376aeae7e6dc4
SoumyaS09 <shetty.soumya09@yahoo.in>
2024-02-28 19:48:49 +0530

    Create employee_grade6.sql

---
+++ employee_grade6/employee_grade6/src/main/resources/employee_grade6.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees VALUES(1,'Finding','Nemo','Nemo@email.com');
+INSERT INTO employees VALUES(2,'Peace','Harmony','Peace@email.com');
+INSERT INTO employees VALUES(3,'Varun','Shashtri','Varun@email.com');
+INSERT INTO employees VALUES(4,'Peter','DSouza','Peter@email.com');
+INSERT INTO employees VALUES(5,'Pradeep','Kumar','PK@email.com');
+INSERT INTO employees VALUES(6,'Roshan','Rakesh','r2r@email.com');
+INSERT INTO employees VALUES(7,'John','Fernandes','John@email.com');
