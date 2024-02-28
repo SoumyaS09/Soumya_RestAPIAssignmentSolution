845ee29cdc16d3f3b69b9514defa3b4a1b3ddb40
SoumyaS09 <shetty.soumya09@yahoo.in>
2024-02-28 19:48:21 +0530

    Create EmployeeGrade6Application.java

---
+++ employee_grade6/employee_grade6/src/main/java/com/great/learning/EmployeeGrade6Application.java
@@ -0,0 +1,14 @@
+package com.great.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeGrade6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeGrade6Application.class, args);
+		System.out.println("Welcome to Employee Management");
+	}
+
+}
